dd73f22557a7f3be0f3cee949bc3cf1f321ba79b
Roman Kayan BAZG <roman.kayan@bazg.admin.ch>
2026-01-20 15:42:23 +0100

    hard-coded subpath /webui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -104,7 +104,7 @@
 			reconnectionDelay: 1000,
 			reconnectionDelayMax: 5000,
 			randomizationFactor: 0.5,
-			path: '/ws/socket.io',
+			path: '/webui/ws/socket.io',
 			transports: enableWebsocket ? ['websocket'] : ['polling', 'websocket'],
 			auth: { token: localStorage.token }
 		});
